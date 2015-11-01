857d9853054cfdb22620394b2e0ad55d6fcc2daf
Dan Walker <dwalker109@gmail.com>
2015-11-01 03:04:20 +0000

    Cleans up some commenting

--- src/Solver.php
+++ src/Solver.php
@@ -52,24 +52,29 @@ class Solver
     */
     private function buildRpnEquations(Array $numbers, $level = 0, $equation = null)
     {
+        // At least two operands deep, iterate and append each operator to this tree
         if ($level >= 2) {
             foreach (Rpn::$operators as $operator) {
                 $this->buildRpnEquations($numbers, $level - 1, $equation . Rpn::OP_TOK . $operator);
             }
         }
 
+        // Iterate all source numbers once per tree
         $all_used = true;
-
         for ($i = 0; $i < count($numbers); $i++) {
             if ($numbers[$i]) {
+                // Flag that all numbers have not been used, iterate further down this tree
+                // after removing current number from available pool and incrementing depth
                 $all_used = false;
                 $n = $numbers[$i];
                 $numbers[$i] = null;
                 $this->buildRpnEquations($numbers, $level + 1, $equation . Rpn::OP_TOK . $n);
+                // Restore current number for use with next iteration's tree
                 $numbers[$i] = $n;
             }
         }
 
+        // No numbers were left and level is 1, this tree is complete
         if ($all_used && $level == 1) {
             $this->rpn_equations[] = trim($equation);
         }
